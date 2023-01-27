701cae3e2db624d44a369d07a647e4d9a94e415a
Pongpanot Chantana <75673766+munichMW@users.noreply.github.com>
2023-01-27 20:07:31 +0700

    Update unit-converter.py

--- unit-converter.py
+++ unit-converter.py
@@ -1,6 +1,32 @@
 import math
 
 units = {
+    # base unit
+    "meter": 0,
+    "gram": 0,
+    "liter": 0,
+    "ton": 0,
+    "second": 0,
+    "ampere": 0,
+    "candela": 0,
+    "mole": 0,
+    "hertz": 0,
+    "newton": 0,
+    "pascal": 0,
+    "watt": 0,
+    "volt": 0,
+    "farad": 0,
+    "ohm": 0,
+    "siemens": 0,
+    "weber": 0,
+    "tesla": 0,
+    "henry": 0,
+    "lumen": 0,
+    "lux": 0,
+    "gallon": 0,
+  
+  # prefix  
+    "quetta": 30,
     "quetta": 30,
     "ronna": 27,
     "yotta": 24,
@@ -13,7 +39,6 @@ units = {
     "kilo": 3,
     "hecto": 2,
     "deca": 1,
-    "normal": 0,
     "deci": -1,
     "centi": -2,
     "milli": -3,
@@ -24,29 +49,26 @@ units = {
     "atto": -18,
     "zepto": -21,
     "yocto": -24,
-    "ronto": -27,
+    "ronto": -27,	
     "quecto": -30
 
 }
-
 
 def checki(question):
     prompt = input(question)
     split_word = None
     for unit in units:
-        if unit in prompt:
+        if prompt.startswith(unit):
             split_word = [unit, prompt[len(unit):], prompt]
             break
     return split_word
 
-
 def sci_notation(num):
-    power = 0
-    while abs(num) >= 10:
-        num /= 10
-        power += 1
-    return num, power
-
+  power = 0
+  while abs(num) >= 10:
+    num /= 10
+    power += 1
+  return num,power
 
 numb = float(input("amount: "))
 default_unit = checki("default_unit: ")
@@ -56,10 +78,11 @@ newdefault_unit = units.get(default_unit[0], None)
 newnew_unit = units.get(new_unit[0], None)
 
 if newdefault_unit is not None and newnew_unit is not None:
-    calcu = numb * math.pow(10, (newdefault_unit - newnew_unit))
+    calcu = numb * math.pow(10,(newdefault_unit - newnew_unit))
     scinonum = sci_notation(numb)
+    if newdefault_unit > newnew_unit:
+        calcu = numb / math.pow(10,(newnew_unit - newdefault_unit))
     print(calcu, new_unit[2],)
-    print(scinonum[0], "x 10 ^",
-          (scinonum[1] + (newdefault_unit - newnew_unit)))
+    print(scinonum[0],"x 10 ^",(scinonum[1] + (newdefault_unit - newnew_unit)))
 else:
     print("Invalid unit selected.")
