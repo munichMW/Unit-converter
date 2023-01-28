d4b39769fd6c459477d03345cb03f08d89b97b7f
Pongpanot Chantana <75673766+munichMW@users.noreply.github.com>
2023-01-28 17:37:11 +0700

    Update unit-converter.py

--- unit-converter.py
+++ unit-converter.py
@@ -1,16 +1,4 @@
-import math
-
 units = {
-    # base unit
-    "meter": 0,
-    "gram": 0,
-    "second": 0,
-    "ampere": 0,
-    "watt": 0,
-    "volt": 0,
-    "ohm": 0,
-  
-  # prefix  
     "quetta": 30,
     "quetta": 30,
     "ronna": 27,
@@ -36,36 +24,78 @@ units = {
     "yocto": -24,
     "ronto": -27,	
     "quecto": -30
-
 }
 
-def checki(question):
-    prompt = input(question)
-    split_word = None
+
+conversion = {
+    "none_prefix": {
+        "": 1
+    },
+    "length": {
+        "meter": 1,
+        "mile": 0.00062137,
+        "inch": 39.3700787,
+        "foot": 3.2808399,
+        "yard": 1.0936133,
+        "AU": 1 / 149597870691,
+        "ly": 1 / 9460730777119564,
+        "pc": 1 / 30856775814671900,
+        "เส้น": 2,
+        "โยชน์": 1,
+        "วา": 2,
+        "ศอก": 0.5,
+        "คืบ": 0.25,
+        "นิ้ว": 1 / 48,
+        "ฝ่ามือ": 1 / 12,
+    },
+    "mass": {
+        "gram": 1000,
+        "pound": 2.20462262,
+        "ounce": 35.27396195,
+        "stone": 0.157473,
+        "carat": 5000,
+        "amu": (1000 / (1.66053886 * (10 ** -24))),
+        "newton": (1 / 101.9716212978) * 1000
+    },
+    "time": {
+        "second": 86400,
+        "minute": 1440,
+        "hour": 24,
+        "day": 1
+    },
+}
+
+def checki(prompt):
+    split_word = [prompt, prompt, prompt]
     for unit in units:
         if prompt.startswith(unit):
             split_word = [unit, prompt[len(unit):], prompt]
             break
     return split_word
 
-def sci_notation(num):
-  power = 0
-  while abs(num) >= 10:
-    num /= 10
-    power += 1
-  return num,power
 
-numb = float(input("amount: "))
-default_unit = checki("default_unit: ")
-new_unit = checki("new_unit: ")
+def convert_unit(value, from_unit_pre, to_unit_pre, from_unit, to_unit):
+    fpre = units.get(from_unit_pre)
+    if fpre is None:
+        fpre = 0
+        
+    tpre = units.get(to_unit_pre)
+    if tpre is None:
+        tpre = 0
+        
+    print(fpre,tpre)
+    
 
-newdefault_unit = units.get(default_unit[0], None)
-newnew_unit = units.get(new_unit[0], None)
 
-if newdefault_unit is not None and newnew_unit is not None:
-    calcu = numb * math.pow(10,(newdefault_unit - newnew_unit))
-    scinonum = sci_notation(numb)
-    print(calcu, new_unit[2],)
-    print(scinonum[0],"x 10 ^",(scinonum[1] + (newdefault_unit - newnew_unit)))
-else:
-    print("Invalid unit selected.")
+
+    for category in conversion:
+        if from_unit in conversion[category] and to_unit in conversion[category]:
+            return value * ((conversion[category][to_unit] * 10 ** float(fpre)) / (conversion[category][from_unit] * 10 **  float(tpre)))
+    return "incorrect"
+
+
+a = float(input("a: "))
+b = checki(input("from: "))
+c = checki(input("to: "))
+print(b,c, units.get(c[0]),units.get(b[0]))
+print(convert_unit(a, b[0], c[0],b[1],c[1]),c[2]) 
